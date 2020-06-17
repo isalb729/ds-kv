9df1b7083824ffa0f401d234531edcde6618ca37
blasi <blasi@vip.qq.com>
2020-06-17 11:37:42 +0800

    init

---
+++ src/utils/config.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"flag"
+	"gopkg.in/yaml.v2"
+	"log"
+	"os"
+)
+
+func readYamlFile(cfg interface{}, file string) error {
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+
+	decoder := yaml.NewDecoder(f)
+	err = decoder.Decode(cfg)
+	if err != nil {
+		return err
+	}
+	return f.Close()
+}
+
+func LoadConfig(cfg interface{}) error {
+	cfgfile := flag.String("cfg", "", "config file")
+	flag.Parse()
+	err := readYamlFile(cfg, *cfgfile)
+	if err != nil {
+		log.Println("fail to parse the config file:", err)
+		return err
+	}
+	return nil
+}
